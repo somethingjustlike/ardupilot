8d29ef75eabc969e96d634e6cc9547d00ca9922a
somethingjustlike <88530537+somethingjustlike@users.noreply.github.com>
2026-09-21 11:01:18 +0800

    Update AP_Filesystem_FlashMemory_LittleFS.cpp

--- libraries/AP_Filesystem/AP_Filesystem_FlashMemory_LittleFS.cpp
+++ libraries/AP_Filesystem/AP_Filesystem_FlashMemory_LittleFS.cpp
@@ -623,6 +623,8 @@ void AP_Filesystem_FlashMemory_LittleFS::mark_dead()
 #define JEDEC_ID_FMSH_FM25Q128A        0xA14018
 #define JEDEC_ID_FMSH_FM25Q256         0xA14019
 #define JEDEC_ID_XTX_XT25F128F         0x0B4018
+#define JEDEC_ID_Giantec_GT25Q64EZ     0x1C7117
+#define JEDEC_ID_Giantec_GT25Q128EZ    0x1C7118
 
 /* Hardware-specific constants */
 
